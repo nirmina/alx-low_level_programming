6765b1c6b7c2484e49cc313c0020285b78b1ed35
nirmina <nirminahiani12@gmail.com>
2023-08-10 18:52:45 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Write a C program that prints a line (fputs)
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
