ed4a7148fe20e0499bb2b1d0108e30a93125e6df
nirmina <nirminahiani12@gmail.com>
2023-08-10 17:49:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main : Write a C program that prints a line
+ * Return : 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
