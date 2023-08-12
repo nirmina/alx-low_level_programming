3536dd6d765d195221617ed28caaa04ade33aa26
nirmina <nirminahiani12@gmail.com>
2023-08-11 19:32:13 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main-Entry Code
+ * Description: a program that print a combination of two two-digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int firtDigit = 0, secondDigit;
+while (firstDigit <= 99)
+{
+secondDigit = firstDigit;
+while (secondDigit <= 99)
+{
+if (secondDigit != firstDigit)
+{
+putchar((firstDigit / 10) + 48);
+putchar((firstDigit % 10) + 48);
+putchar(' ');
+putchar((secondDigit / 10) + 48);
+putchar((secondDigit % 10) + 48);
+if (firstDigit != 98 || secondDigit != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+secondDigit++;
+}
+firstDigit++;
+}
+putchar('\n');
+return (0);
+}
