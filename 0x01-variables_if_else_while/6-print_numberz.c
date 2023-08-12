02295dee385019503bcbce5be9d6bd7a2a50ed07
nirmina <nirminahiani12@gmail.com>
2023-08-11 18:22:47 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * Description: print 0-9 using putchar while using intVar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar(digit + '0');
+digit++;
+}
+putchar('\n');
+return (0);
+}
