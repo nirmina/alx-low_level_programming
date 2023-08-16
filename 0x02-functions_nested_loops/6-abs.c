53e5324a7d0eeaa207bae63649185d763e88642c
nirmina <nirminahiani12@gmail.com>
2023-08-15 18:54:35 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - a functio that give an absolute of anumber
+ * @n: takes an integer type of functions
+ * Return: 0 (success)
+ */
+int _abs(int n)
+{
+if (n < 0)
+n = (-1) * n;
+return (n);
+}
