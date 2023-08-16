c0c2baae0a4da3a7fec9708b2fe6bd941f215ba4
nirmina <nirminahiani12@gmail.com>
2023-08-15 18:05:16 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - print the alphabet form a to z
+ */
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a'; ch < 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
