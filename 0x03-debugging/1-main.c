4d6fc98907f59bba4dabea357b7174371d272c9d
nirmina <nirminahiani12@gmail.com>
2023-08-17 14:29:52 -0700

    1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Demonstrates an infinite loop and how to avoid it.
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*
+ * Infinite loop: while (i < 10)
+  {
+   putchar(i);
+   }
+*/
+
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
+
