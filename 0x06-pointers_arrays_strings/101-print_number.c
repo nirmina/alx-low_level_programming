dfe3f246c385f922ebfd130163256dccb42713a6
nirmina <nirminahiani12@gmail.com>
2023-08-23 18:29:42 -0700

    n

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number -  a function that prints an integer
+ * @n: int
+ * Return: -
+ */
+void print_number(int n)
+{
+unsigned int i;
+i = n;
+if (n < 0)
+{
+_putchar('-');
+i = -n;
+}
+if (i / 10 != 0)
+{
+print_number(i / 10);
+}
+_putchar((i % 10) + '0');
+}
