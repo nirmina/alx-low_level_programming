476aa2e7d7e7d4c4177e8a400bd693edf5a94314
nirmina <nirminahiani12@gmail.com>
2023-08-30 11:31:02 -0700

    7

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * square - find suare root
- * @val1: to find suare root
- * @val2: square root
- * Return: integer
- */
-int square(int val1, int val2)
-{
-if (val1 * val1 == val2)
-return (val1);
-else if (val1 * val1 < val2)
-return (square(val2, val1 + 1));
-else
-return (-1);
-}
-/**
  * _sqrt_recursion - a function that returns the natural square root of a numbe
  * @n: number to calculate square root
  * Return: the resulting square
+ * square - find suare root (the second function)
+ * @n: to find square root function square
+ * @val2: square root of the second function square
  */
+int square(int n, int val2);
 int _sqrt_recursion(int n)
 {
 return (square(n, 1));
+}
+/**
+ * square - find suare root
+ * @n: to find suare root
+ * @val2: square root
+ * Return: integer
+ */
+int square(int n, int val2)
+{
+if (val2 * val2 == n)
+return (val2);
+else if (val2 * val2 < n)
+return (square(n, val2 + 1));
+else
+return (-1);
 }
