15cd4850f1766662bf6d99c871aba6cf8cbf9fd5
nirmina <nirminahiani12@gmail.com>
2023-09-07 18:29:58 -0700

    2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,3 +11,30 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+void *s;
+unsigned int i;
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+{
+s = malloc(new_size);
+if (s == NULL)
+return (NULL);
+return (s);
+}
+if (new_size >= old_size)
+{
+s = malloc(new_size);
+if (s == NULL)
+return (NULL);
+for (i = 0; i < old_size && i < new_size; i++)
+*((char *)s + i) = *((char *)ptr + i);
+free(ptr);
+}
+return (s);
+}
