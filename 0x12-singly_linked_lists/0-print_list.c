dfb5fab8a3b883183e248bf6b956c2d3a727840e
nirmina <nirminahiani12@gmail.com>
2023-09-21 16:50:58 -0700

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "lists.h"
 /**
+ * _strlen - Calculates the length of a null-terminated string.
+ * @str: A pointer to the null-terminated string to calculate the length of.
+ * Return: The length of the string.
+ */
+int _strlen(const char *str)
+{
+int length = 0;
+if (!str)
+return (0);
+while (*str++)
+length++;
+return (length);
+}
+/**
  * print_list -  a function that prints all the elements of a list_t list
  * @h: pointer to the list_t list to print
  * Return: the number of nodes
@@ -10,10 +24,7 @@ size_t print_list(const list_t *h)
 size_t s = 0;
 while (h)
 {
-if (!h->str)
-printf("[0](nil)\n");
-else
-printf("[%u]%s\n", h->len, h->str);
+printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 h = h->next;
 s++;
 }
