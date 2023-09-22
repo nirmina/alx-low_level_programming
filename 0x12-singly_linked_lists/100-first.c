4d220a26544359fd102e0f378ba0f1fb51118948
nirmina <nirminahiani12@gmail.com>
2023-09-21 18:00:55 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void first(void)__attribute__((constructor));
+/**
+ * first - a function that prints You're beat! and yet, you must allow,
+ * \nI bore my house upon my back!\n before the main function is executed.
+ * fnction is executed
+ */
+void first(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
