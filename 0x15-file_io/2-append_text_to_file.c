ff52915777c00ad0cc257ef8d5f0ae78476b17ee
nirmina <nirminahiani12@gmail.com>
2023-10-02 17:31:45 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+ * _strlen - returns the lenght of a string
+ * @s: the string whose lenght to check
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+int i = 0;
+if (!s)
+return (0);
+while (*s++)
+i++;
+return (i);
+}
 /**
  * append_text_to_file - a function that appends text at the end of a file.
  * @filename:  is the name of the file
@@ -9,4 +27,16 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-
+int file;
+ssize_t bytes = 0;
+ssize_t lenght = _strlen(text_content);
+if (!filename)
+return (-1);
+file = open(filename, O_WRONLY | O_APPEND);
+if (file == -1)
+return (-1);
+if (lenght)
+bytes = write(file, text_content, lenght);
+close(file);
+return (bytes == lenght ? 1 : -1);
+}
---
+++ 0x15-file_io/_strlen.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the lenght of a string
+ * @s: the string whose lenght to check
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+int i = 0;
+if (!s)
+return (0);
+while (*s++)
+i++;
+return (i);
+}
