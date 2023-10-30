925398bdfea9ba7df147e80796f0c038ea92637c
nirmina <nirminahiani12@gmail.com>
2023-10-30 16:49:28 -0700

    first task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,29 +2,29 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 /**
- * read_textfile -  it reads a text file and prints it to the POSIX standoutput
- * @filename: text file being read
+ * read_textfile - a funcyion that it read  a text file and prints
+ * it to the POSIX standoutput
+ * @filename: a text file being read
  * @letters: is the number of letters it should read and print
- * Return: if the file can not be opened or read, return 0
- * if filename is NULL return 0
+ * Return: if the file can not be opened or read it return (0)
+ * if the filename is null it retun (0)
  * if write fails or does not write the expected amount of bytes, return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buf;
+char *thebuffer;
 ssize_t file;
-ssize_t w;
-ssize_t s;
+ssize_t w, s;
 file = open(filename, O_RDONLY);
 if (file == -1)
 return (0);
-buf = malloc(sizeof(char) * letters);
-s = read(file, buf, letters);
-w = write(STDOUT_FILENO, buf, s);
-free(buf);
+thebuffer = malloc(sizeof(char) * letters);
+s = read(file, thebuffer, letters);
+w = write(STDOUT_FILENO, thebuffer, s);
+free(thebuffer);
 close(file);
 return (w);
 }
