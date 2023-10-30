991b7bf719aa6a9bef2650aca5b952768156cf76
nirmina <nirminahiani12@gmail.com>
2023-10-30 16:51:55 -0700

    second task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,15 +4,14 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * create_file - a function that creates a file.
- * @filename:  is the name of the file to create
+ * create_file - its a function that creates a file
+ * @filename:  is the name of the file that it needs to be create
  * @text_content:  is a NULL terminated string to write to the file
- * Return: 1 (success) , -1 (failure)
+ * Return: 1 (mean success) , -1 (failure)
  */
 int create_file(const char *filename, char *text_content)
 {
-int file;
-int w;
+int file, w;
 int lenght = 0;
 if (filename == NULL)
 return (-1);
